f5ffd05c15fd1a142246e0bfcffb5f698a168855
Sushil Jain <sushil.jain01@sap.com>
2020-05-12 16:12:40 +0200

    added a new function named sayHello which accepts a name and prints hello {name} to stdout

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -2,4 +2,8 @@ public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello motu");
     }
+    
+    public void sayHello(String name) {
+    	System.out.println("Hello "+name);
+    }
 }
