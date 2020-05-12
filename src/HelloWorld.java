5f5543eafbad114c058619c0f2db10e34e24ee24
Shalu Singhal <singhal.shalu571@gmail.com>
2020-05-12 16:18:47 +0200

    some experiment

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,5 +1,19 @@
 public class HelloWorld {
+
+    public void invokeSayHello() {
+        new MySecondClass().sayHello();
+    }
     public static void main(String[] args) {
         System.out.println("Hello motu");
+        System.out.println(Math.sqrt(9));
+        new HelloWorld().invokeSayHello();
+
+    }
+}
+
+
+class MySecondClass {
+    public void sayHello() {
+        System.out.println("I'm saying hello from MySecondClass");
     }
 }
